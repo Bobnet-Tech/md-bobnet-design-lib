5e4eaa54503c0fb00e78767bf5da271c10271c07
DariusGalu <darius.galu@bobnet.ro>
2022-09-16 09:09:25 +0300

    TDD-3115 Added another button for pop-ups

--- example/storybook/stories/Popups/Popup.stories.js
+++ example/storybook/stories/Popups/Popup.stories.js
@@ -59,6 +59,73 @@ storiesOf('Popups', module)
               style={{ margin: 10 }}
               onPress={() => showPopup('NeutralFace')}
             />
+            <Button
+              type="primary"
+              text="DeactivateAccount"
+              style={{ margin: 10 }}
+              onPress={() => showPopup('DeactivateAccount')}
+            />
+          </View>
+        </PopupRoot>
+      );
+    })
+  )
+  .add('DeleteAccount', () =>
+    React.createElement(() => {
+      const showPopup = (type) => {
+        Popup.show({
+          type: type,
+          title: 'Sigur vrei sa iti \ndezactivezi contul?',
+          button: true,
+          secondButton: true,
+          textBody: '',
+          buttonText: 'Nu, continua',
+          secondButtonText: 'Dezactiveaza',
+          callback: () => Popup.hide(),
+          secondCallback: () => Popup.hide(),
+        });
+      };
+
+      return (
+        <PopupRoot>
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Button
+              type="primary"
+              text="DeactivateAccount"
+              style={{ margin: 10 }}
+              onPress={() => showPopup('DeactivateAccount')}
+            />
+          </View>
+        </PopupRoot>
+      );
+    })
+  )
+  .add('DeleteAddress', () =>
+    React.createElement(() => {
+      const showPopup = (type) => {
+        Popup.show({
+          type: type,
+          title:
+            'Strada Campia Libertatii nr.200, Bl.10, Scara B, Et.6, Ap.01, Sector 3',
+          button: true,
+          secondButton: true,
+          textBody: 'Sigur vrei sa stergi adresa?',
+          buttonText: 'Da',
+          secondButtonText: 'Renunta',
+          callback: () => Popup.hide(),
+          secondCallback: () => Popup.hide(),
+        });
+      };
+
+      return (
+        <PopupRoot>
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Button
+              type="primary"
+              text="DeleteAccount"
+              style={{ margin: 10 }}
+              onPress={() => showPopup('DeleteAddress')}
+            />
           </View>
         </PopupRoot>
       );
