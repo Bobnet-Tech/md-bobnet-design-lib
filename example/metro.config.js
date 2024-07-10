b334afb9ce5d99f542b8678ce89b3bf109a6fe9f
Remus Balan <bnt.remus.balan>
2024-07-10 15:41:02 +0300

    TDD-5898 Upgrade Bob Design Library to React Native 0.73.8

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,26 +1,50 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const path = require('path');
+const path = require("path");
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const pak = require('../package.json');
-
+const blacklist = require('metro-config/src/defaults/exclusionList');
+const defaultConfig = getDefaultConfig(__dirname);
+const { resolver: { sourceExts, assetExts } } = defaultConfig;
 const root = path.resolve(__dirname, '..');
-
 const modules = Object.keys({
   ...pak.peerDependencies,
 });
 
-const baseConfig = getDefaultConfig(__dirname);
-
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 const config = {
-  ...baseConfig,
-  watchFolders: [root],
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
   resolver: {
-    ...baseConfig.resolver,
-    resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
+    assetExts: assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"],
+    resolverMainFields: [
+      "sbmodern",
+      ...defaultConfig.resolver.resolverMainFields,
+    ],
+    blacklistRE: blacklist(
+      modules.map(
+        (m) =>
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      )
+    ),
+
     extraNodeModules: modules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
     }, {}),
   },
+  watchFolders: [path.resolve(__dirname, "../")],
 };
 
-module.exports = mergeConfig(baseConfig, config);
+module.exports = mergeConfig(defaultConfig, config);
